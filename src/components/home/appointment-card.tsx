0b10548fdae016e9efee4fea399605c191c41e7b
aluxf <alexfooladi@hotmail.com>
2025-01-25 22:55:00 +0100

    add appointments to home screen

--- src/components/home/appointment-card.tsx
+++ src/components/home/appointment-card.tsx
@@ -10,19 +10,24 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-
-export function AppointmentCard() {
-  return (
-    <Card className="w-[350px] p-0">
-      <CardHeader className="flex flex-row space-y-0 p-5">
-        <div className="flex flex-col gap-2">
-            <CardTitle>Drug Perscription</CardTitle>
-            <CardDescription>Dr. Alex Fooladi</CardDescription>
-        </div>
-        <div className="ml-auto">
-            <CardDescription>2025-01-24</CardDescription>
-        </div>
-      </CardHeader>
-    </Card>
-  )
+interface AppointmentCardProps {
+    title: string
+    doctor: string
+    date: string
 }
+
+export function AppointmentCard({ title, doctor, date }: AppointmentCardProps) {
+    return (
+      <Card className="w-[350px] p-0">
+        <CardHeader className="flex flex-row space-y-0 p-5">
+          <div className="flex flex-col gap-2">
+              <CardTitle>{title}</CardTitle>
+              <CardDescription>{doctor}</CardDescription>
+          </div>
+          <div className="ml-auto">
+              <CardDescription>{date}</CardDescription>
+          </div>
+        </CardHeader>
+      </Card>
+    )
+  }
