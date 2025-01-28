a41af9136b34c0b5ce5da674ff431fdbeb7ce953
aluxf <alexfooladi@hotmail.com>
2025-01-28 03:47:01 +0100

    user auth done

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -97,7 +97,7 @@ export function LoginForm({
           </div>
           <div className="text-center text-sm">
             Don&apos;t have an account?{" "}
-            <a href="#" className="underline underline-offset-4">
+            <a href="#" onClick={() => router.push("/register")} className="underline underline-offset-4">
               Sign up
             </a>
           </div>
